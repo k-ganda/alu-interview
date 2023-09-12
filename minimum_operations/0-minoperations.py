24a6f7126d7564c1285cf4c02c9f28652285176e
k-ganda <k.ganda@alustudent.com>
2023-09-12 13:00:35 -0700

    minnimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,32 +6,25 @@ operations required to reach a given number
 
 
 def minOperations(n):
-    """
-    Calculates the minimum number of operations required
-    to reach a given number using the following operations:
-     - Add 1
-     - Multiply by 2
+    """Calculation  of operations needed to obtain exactly n H characters
 
     Args:
-        n (int): The target number.
+        n (int): The target number of H characters.
 
     Returns:
-        int: The minimum number of operations required to reach the target number.
+        int: The minimum number of operations required.
     """
+
     if n <= 1:
         return 0
 
-    operations = [0] * (n + 1)
-
-    for i in range(2, n + 1):
+    operations = 0
+    i = 2
+    while i <= n:
         if n % i == 0:
-            operations[i] = i
-            continue
-        j = i - 1
-        while j > 1:
-            if i % j == 0:
-                operations[i] = operations[j] + (i // j)
-                break
-            j -= 1
+            operations += i
+            n //= i
+        else:
+            i += 1
 
     return operations[n]
